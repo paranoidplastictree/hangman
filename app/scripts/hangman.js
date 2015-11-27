51b55a8e312c086ee5f478c3c4b47cf2ee9a09e9
Tim Storms <tstorms@springthrough.com>
2015-11-27 07:26:47 -0500

    added css and built out game logic

--- app/scripts/hangman.js
+++ app/scripts/hangman.js
@@ -1,10 +1,3 @@
-var Guess = (function () {
-    function Guess(success, letter) {
-        this.success = success;
-        this.value = letter;
-    }
-    return Guess;
-})();
 var Member;
 (function (Member) {
     Member[Member["Head"] = 0] = "Head";
@@ -16,21 +9,85 @@ var Member;
 })(Member || (Member = {}));
 var Gallows = (function () {
     function Gallows() {
-        this.gallowsDiv = document.getElementById("gallows");
+        this.bodyState = Member.Head;
+        this.stickman = document.getElementById("stickman");
+        this.context = this.stickman.getContext('2d');
+        this.context.fillStyle = "yellow";
+        this.context.clearRect(0, 0, this.stickman.width, this.stickman.height);
+        this.context.beginPath();
+        this.drawGallows();
     }
+    Gallows.prototype.drawGallows = function () {
+        // base
+        this.context.moveTo(50, 140);
+        this.context.lineTo(90, 140);
+        this.context.stroke();
+        // upright
+        this.context.moveTo(70, 140);
+        this.context.lineTo(70, 4);
+        this.context.stroke();
+        // horizontal beam
+        this.context.moveTo(70, 4);
+        this.context.lineTo(150, 4);
+        this.context.stroke();
+        // noose
+        this.context.moveTo(150, 4);
+        this.context.lineTo(150, 10);
+        this.context.stroke();
+    };
     Gallows.prototype.drawNextMember = function () {
-        if (this.bodyState < Member.RightLeg) {
-            this.bodyState = this.bodyState + 1;
+        switch (this.bodyState) {
+            case Member.Head:
+                this.context.beginPath();
+                this.context.arc(150, 30, 20, 0, 2 * Math.PI);
+                this.context.closePath();
+                this.context.fill();
+                this.context.stroke();
+                break;
+            case Member.Torso:
+                this.context.moveTo(150, 50);
+                this.context.lineTo(150, 100);
+                this.context.stroke();
+                break;
+            case Member.LeftArm:
+                this.context.moveTo(150, 60);
+                this.context.lineTo(200, 70);
+                this.context.stroke();
+                break;
+            case Member.RightArm:
+                this.context.moveTo(150, 60);
+                this.context.lineTo(100, 70);
+                this.context.stroke();
+                break;
+            case Member.LeftLeg:
+                this.context.moveTo(150, 100);
+                this.context.lineTo(200, 120);
+                this.context.stroke();
+                break;
+            case Member.RightLeg:
+                this.context.moveTo(150, 100);
+                this.context.lineTo(100, 120);
+                this.context.stroke();
+                break;
+            default:
+                break;
         }
+        this.bodyState = this.bodyState + 1;
     };
     return Gallows;
 })();
 var Game = (function () {
-    function Game(secret, guesses) {
-        if (guesses === void 0) { guesses = null; }
+    function Game(secret) {
         this.secret = secret;
         this.mask = secret.split('');
-        this.guesses = guesses;
+        this.gallows = new Gallows();
+        this.incorrectGuesses = new Array();
+        var input = document.getElementById("guess-input");
+        var missesDiv = document.getElementById("misses");
+        var results = document.getElementById("results");
+        missesDiv.innerHTML = "";
+        results.style.display = "none";
+        input.focus();
         this.mask = this.mask.map(function (char) {
             if (char.match(/[a-z]/i)) {
                 return "_";
@@ -41,6 +98,8 @@ var Game = (function () {
             else
                 return char;
         });
+        var maskDiv = document.getElementById("mask");
+        maskDiv.innerHTML = this.mask.join(" ");
     }
     Game.prototype.validateGuess = function () {
         var input = document.getElementById("guess-input");
@@ -50,25 +109,45 @@ var Game = (function () {
             // success!!
             for (var i = 0; i < this.secret.length; i++) {
                 var index = this.secret.indexOf(input.value, i);
-                if (index > 0) {
+                if (index >= 0) {
                     this.mask[index] = input.value;
                 }
             }
             maskDiv.innerHTML = this.mask.join(" ");
+            if (this.mask.toString().indexOf("_") < 0) {
+                this.showResults(true);
+            }
         }
-        else if (input.value.length !== 1) {
+        else if (input.value.length > 1) {
             alert('invalid guess!');
         }
         else {
-            this.gallows.drawNextMember();
-            missesDiv.innerHTML = missesDiv.innerHTML.concat(input.value);
+            var str = this.incorrectGuesses.join("");
+            if (str.indexOf(input.value) < 0) {
+                this.incorrectGuesses.push(input.value);
+                this.gallows.drawNextMember();
+                missesDiv.innerHTML = this.incorrectGuesses.toString();
+                if (this.gallows.bodyState == Member.RightLeg + 1) {
+                    this.showResults(false);
+                }
+            }
         }
         input.value = "";
+    };
+    Game.prototype.showResults = function (success) {
+        var results = document.getElementById("results");
+        if (success) {
+            results.innerHTML = "You guessed it!";
+        }
+        else {
+            results.innerHTML = "Better luck next time :/";
+        }
+        results.style.display = "block";
     };
     return Game;
 })();
 var game = new Game("nacho libre!");
-function submitGuess() {
+function submitGuess(e) {
     game.validateGuess();
 }
 function newGame() {
--- app/scripts/hangman.ts
+++ app/scripts/hangman.ts
@@ -1,71 +1,169 @@
-class Guess {
-	success: boolean;
-	value: string;
-	constructor(success: boolean, letter: string){
-		this.success = success;
-		this.value = letter;
-	}
-}
 enum Member {Head, Torso, LeftArm, RightArm, LeftLeg, RightLeg}
 class Gallows {
 	bodyState: Member;
-	gallowsDiv: HTMLElement;
+	stickman: HTMLCanvasElement;
+	context: CanvasRenderingContext2D;
+	
 	constructor(){
-		this.gallowsDiv = document.getElementById("gallows");
+		this.bodyState = Member.Head;
+		this.stickman = <HTMLCanvasElement>document.getElementById("stickman");
+		this.context = this.stickman.getContext('2d');
+		this.context.fillStyle = "yellow";
+		this.context.clearRect(0, 0, this.stickman.width, this.stickman.height);
+		this.context.beginPath();
+		this.drawGallows();
+	}
+	
+	drawGallows(){
+    	// base
+		this.context.moveTo(50, 140);
+		this.context.lineTo(90, 140);
+		this.context.stroke();
+		
+		// upright
+		this.context.moveTo(70, 140);
+		this.context.lineTo(70, 4);
+		this.context.stroke();
+		
+		// horizontal beam
+		this.context.moveTo(70, 4);
+		this.context.lineTo(150, 4);
+		this.context.stroke();
+  
+		// noose
+		this.context.moveTo(150,4);
+		this.context.lineTo(150,10);
+		this.context.stroke();
 	}
 	
 	drawNextMember(){
-		if(this.bodyState<Member.RightLeg){
-			this.bodyState = this.bodyState + 1;
+		switch(this.bodyState){
+			case Member.Head:
+				this.context.beginPath();
+				this.context.arc(150, 30, 20, 0, 2*Math.PI);
+				this.context.closePath();
+				this.context.fill();
+				this.context.stroke();
+				break;
+			case Member.Torso:
+				this.context.moveTo(150, 50);
+				this.context.lineTo(150, 100);
+				this.context.stroke();
+				break;
+			case Member.LeftArm:
+				this.context.moveTo(150, 60);
+				this.context.lineTo(200, 70);
+				this.context.stroke();
+				break;
+			case Member.RightArm:
+				this.context.moveTo(150, 60);
+				this.context.lineTo(100, 70);
+				this.context.stroke()
+				break;
+			case Member.LeftLeg:
+				this.context.moveTo(150, 100);
+				this.context.lineTo(200, 120);
+				this.context.stroke();
+				break;
+			case Member.RightLeg:
+				this.context.moveTo(150, 100);
+				this.context.lineTo(100, 120);
+				this.context.stroke();
+				break;
+			default:
+				break;
 		}
+		
+		this.bodyState = this.bodyState + 1;
 	}
 }
 class Game {
 	mask: Array<string>;
 	secret: string;
-	guesses: Array<Guess>;
+	incorrectGuesses: Array<string>;
 	gallows: Gallows;
 	
-	constructor(secret: string, guesses: Array<Guess> = null){
+	constructor(secret: string){
 		this.secret = secret;
 		this.mask = secret.split('');
-		this.guesses = guesses;
+		this.gallows = new Gallows();
+		this.incorrectGuesses = new Array<string>();
+		
+		var input: HTMLInputElement = <HTMLInputElement>document.getElementById("guess-input");
+		var missesDiv: HTMLElement = document.getElementById("misses");
+		var results = document.getElementById("results");
+		
+		missesDiv.innerHTML = "";
+		results.style.display = "none";
+		input.focus();
+		
 		this.mask = this.mask.map(function(char){
 			if(char.match(/[a-z]/i)){
 				return "_";
 			}
-			else if(char===" "){return "  ";}
+			else if(char === " "){
+				return "  ";
+			}
 			else return char;
 		});
+		
+		var maskDiv: HTMLElement = document.getElementById("mask");
+		maskDiv.innerHTML = this.mask.join(" ");
 	}
 	
 	validateGuess(){
 		var input: HTMLInputElement = <HTMLInputElement>document.getElementById("guess-input");
 		var maskDiv: HTMLElement = document.getElementById("mask");
 		var missesDiv: HTMLElement = document.getElementById("misses");
-	
+		
 		if(this.secret.indexOf(input.value)>=0 && input.value.length===1){
 			// success!!
 			for(var i=0;i<this.secret.length;i++){
 				var index = this.secret.indexOf(input.value, i)
-				if(index>0){
+				if(index>=0){
 					this.mask[index] = input.value;
 				}
 			}
 			maskDiv.innerHTML = this.mask.join(" ");
+			
+			if(this.mask.toString().indexOf("_")<0){
+				this.showResults(true);
+			}
 		}
-		else if(input.value.length!==1){
+		else if(input.value.length>1){
 			alert('invalid guess!');
 		}
-		else{
-			this.gallows.drawNextMember();
-			missesDiv.innerHTML = missesDiv.innerHTML.concat(input.value);
+		else {
+			var str = this.incorrectGuesses.join("");
+			if(str.indexOf(input.value)<0){
+				this.incorrectGuesses.push(input.value);
+				this.gallows.drawNextMember();
+				missesDiv.innerHTML = this.incorrectGuesses.toString();
+				
+				if(this.gallows.bodyState == Member.RightLeg+1){
+					this.showResults(false);
+				}
+			}
 		}
 		input.value = "";
 	}
+	
+	showResults(success){
+		var results = document.getElementById("results");
+		
+		if(success){
+			results.innerHTML = "You guessed it!";	
+		}
+		else{
+			results.innerHTML = "Better luck next time :/";
+		}
+		results.style.display = "block"
+	}
 }
+
 var game = new Game("nacho libre!");
-function submitGuess(){
+
+function submitGuess(e){
 	game.validateGuess();
 }
 function newGame(){
